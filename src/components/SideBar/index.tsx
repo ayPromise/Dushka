fc701bdd6d10cd13f7afd217419fd250dd8e4513
ayPromise servant <fatherpother@gmail.com>
2025-05-12 19:21:59 +0300

    feat: updated SideBar v0.1.1 component

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,11 +1,42 @@
-import AddTodoForm from "./AddTodoForm"
+import { useState } from "react"
+import type { TodoItem } from "../../types/TodoItem"
+import Button from "../Button"
 import SideActions from "./SideActions"
 import styles from "./SideBar.module.scss"
 
-const SideBar = () => {
+interface SideBarProps {
+    handleAddItem: (newItem: TodoItem) => void
+}
+
+const SideBar: React.FC<SideBarProps> = ({ handleAddItem }) => {
+    const [content, setContent] = useState<string>("")
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newValue = event.target.value
+        setContent(newValue)
+    }
+
+    const handleClick = () => {
+        if (content) {
+            const newItem: TodoItem = {
+                content,
+                completed: false
+            }
+            handleAddItem(newItem)
+            setContent("")
+        }
+    }
+
     return (
         <aside className={styles.sidebar}>
-            <AddTodoForm />
+
+            {/** Add todo item */}
+            <form className={styles.addForm}>
+                <h3 className={styles.addTitle}>Add a todo</h3>
+                <input type="text" className={styles.addInput} onChange={handleChange} value={content} placeholder="Study..." />
+                <Button onClick={handleClick}>Add to list</Button>
+            </form>
+
             <SideActions />
         </aside>
     )
