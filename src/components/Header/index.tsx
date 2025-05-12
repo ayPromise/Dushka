a1a6abf0575b3f66ea9a21a18d96281c3a030a0e
ayPromise servant <fatherpother@gmail.com>
2025-05-12 21:02:28 +0300

    chore: some styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,12 @@
+import type { TodoItem } from "../../types/TodoItem"
 import styles from "./Header.module.scss"
 
-const Header = () => {
+interface HeaderProps {
+    todos: TodoItem[]
+}
+
+const Header: React.FC<HeaderProps> = ({ todos }) => {
+    const completedItems = todos.filter((item) => item.completed)
     return (
         <header className={styles.header}>
             <div className={styles.circlesContainer}>
@@ -8,7 +14,7 @@ const Header = () => {
                 <div className={styles.circle}></div>
                 <div className={styles.circle}></div>
             </div>
-            <div><strong>0</strong> / 3 todos completed</div>
+            <div><strong>{completedItems.length}</strong> / {todos.length} completed</div>
         </header>
     )
 }
