307ff832ea5f9e6212e4a26fcd489d37edba05ea
ayPromise servant <fatherpother@gmail.com>
2025-05-12 18:50:00 +0300

    feat: added Footer v0.1.0 component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import styles from "./Footer.module.scss"
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <pre className={styles.span}>
+                I was inspired by&nbsp;
+                <a className={styles.link} href="https://www.youtube.com/watch?v=5r25Y9Vg2P4&t=3288s" target="#blank">ByteGuard video</a>
+            </pre>
+        </footer>
+    )
+}
+
+export default Footer
