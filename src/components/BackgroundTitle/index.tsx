9e67da98ccf68fae45ebf597078226a9fa9f2d12
ayPromise servant <fatherpother@gmail.com>
2025-05-12 18:49:29 +0300

    feat: added BackgroundTitle v0.1.0 component

---
+++ src/components/BackgroundTitle/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./BackgroundTitle.module.scss"
+
+const BackgroundTitle = () => {
+    return (
+        <h1 className={styles.h1}>Dushka</h1>
+    )
+}
+
+export default BackgroundTitle
